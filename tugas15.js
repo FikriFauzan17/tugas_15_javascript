9e666763746a9735e9d8bd62ae288de631bb496f
User-Niomic-Fikri <fikrifauzan17@gmail.com>
2020-06-11 19:13:54 +0700

    Tugas 15 Javascript
    tugas

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+function pytagoras() {
+
+var ab = 8;
+console.log('Nilai AB :',ab);
+var bc = 6;
+console.log('Nilai BC :',bc);
+//rumus pytagoras ckuadrat = akuadrat + bkuadrat
+var ac = Math.pow(ab,2) + Math.pow(bc,2);
+console.log('panjang sisi AC pada segitiga siku-siku tersebut adalah :',Math.sqrt(ac),'cm');
+}
+pytagoras();
